0126c58a051ceedad0f83ef558ad5008e4cdf5b8
PhoenixAsange <phoenixxasange@gmail.com>
2025-02-19 10:00:53 +1000

    Create front-end integration

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,61 +1,87 @@
 let computerScore = 0;
 let playerScore = 0;
 
+const btn = document.querySelector(".choices").addEventListener("click", (event) => {
+  let playerChoice;
+  if (event.target.classList.contains("rock")) {
+    playerChoice = "rock"; 
+  }
+  else if (event.target.classList.contains("paper")) {
+    playerChoice = "paper"; 
+  }
+  else if (event.target.classList.contains("scissors")) {
+    playerChoice = "scissors"; 
+  }
+
+  displayChoice("Player", playerChoice, "choice")
+  playRound(computerChoice(), playerChoice);
+  displayScore(computerScore, playerScore);
+});
+
 
 function computerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
 
   if (computerChoice === 0) {
-    return computerChoice = "rock";
+    computerChoice = "rock";
   } 
   else if (computerChoice === 1) {
-    return computerChoice = "paper";
+    computerChoice = "paper";
   } 
   else if (computerChoice === 2) {
-    return computerChoice = "scissors";
+    computerChoice = "scissors";
   }
+
+  displayChoice("Computer", computerChoice, "choice")
+  return computerChoice;
 }  
-
-function playerChoice() {
-  let playerChoice = prompt("Do you choose rock, paper, or scissors.");
-
-  if (playerChoice.toLowerCase() === "rock" || playerChoice.toLowerCase() === "paper" || playerChoice.toLowerCase() === "scissors") {
-    return playerChoice; 
-  }
-  else {
-    for (;;) {
-      console.log(`${playerChoice} is not a valid input, try again.`);
-      playerChoice = prompt("Do you choose rock, paper, or scissors.");
-      if (playerChoice.toLowerCase() === "rock" || playerChoice.toLowerCase() === "paper" || playerChoice.toLowerCase() === "scissors") {
-        break;
-      }
-    }
-  }
-}
 
 function playRound(computerChoice, playerChoice) {
   if (computerChoice === "rock" && playerChoice === "rock" || computerChoice === "paper" && playerChoice === "paper" || computerChoice === "scissors" && playerChoice === "scissors"){
-    console.log(`Tie! You both chose ${computerChoice}.`);
+    displayChoice(" ", playerChoice, "tie");
   }
 
   else if (computerChoice === "rock" && playerChoice === "scissors" || computerChoice === "scissors" && playerChoice === "paper" || computerChoice === "paper" && playerChoice === "rock"){
-    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    displayChoice(" ", playerChoice, "lose");
     return computerScore += 1;
   }
 
   else if (computerChoice === "scissors" && playerChoice === "rock" || computerChoice === "paper" && playerChoice === "scissors" || computerChoice === "rock" && playerChoice === "paper"){
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+    displayChoice(" ", playerChoice, "win");
     return playerScore += 1;
   }
 }
 
 function displayScore(computerScore, playerScore) {
-  console.log(`Your score: ${playerScore}`)
-  console.log(`Computer score: ${computerScore}`)
+  const player = document.querySelector(".player")
+  player.innerHTML = " ";
+  player.append(`${playerScore}`)
+
+  const computer = document.querySelector(".computer")
+  computer.innerHTML = " ";
+  computer.append(`${computerScore}`)
 }
 
-for (let i = 1; i < 6; i++) {
-  console.log(`Round: ${i}`)
-  playRound(computerChoice(), playerChoice());
-  displayScore(computerScore, playerScore);
+function displayChoice(player, choice, state) {
+  const feedElement = document.createElement("div");
+  feedElement.classList.add("feed");
+  if (state === "choice")
+    feedElement.textContent = `${player} chose ${choice}!`
+  else if (state === "win") {
+    feedElement.style.color = 'green'
+    feedElement.style.fontWeight = 'bold' 
+    feedElement.textContent = `You Win!`
+  }
+  else if (state === "lose") {
+    feedElement.style.color = 'red'
+    feedElement.style.fontWeight = 'bold' 
+    feedElement.textContent = `You Lose!`
+  }
+  else
+    feedElement.textContent = `Tie!`
+  
+  const runningElement = document.querySelector('.running');
+  runningElement.appendChild(feedElement);
 }
+
+
